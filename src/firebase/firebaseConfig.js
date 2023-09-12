54da908f1a568d394ed449c3762898a96b4da564
Michael Jurado <mikjurado1@gmail.com>
2023-09-12 10:22:46 +0200

    fin signup with google

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
